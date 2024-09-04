20868aac322d188f83c3ddd3578d229e4df7c34a
Sukhdev Thukral <xs2ironkid@gmail.com>
2024-09-04 15:58:22 +0530

    slider update

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ document.getElementById("navToggle").addEventListener("click", function() {
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("active");
 });
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
